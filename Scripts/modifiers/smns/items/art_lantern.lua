39456e98260f7702f96aff983e0215bca726fe1b
obscurezz <iamjustobscure@gmail.com>
2025-04-04 13:05:22 +0300

    changed lantern

--- Scripts/modifiers/smns/items/art_lantern.lua
+++ Scripts/modifiers/smns/items/art_lantern.lua
@@ -1,5 +1,6 @@
 package.path = ".\\Scripts\\?.lua;.\\Scripts\\exp\\?.lua;.\\Scripts\\modifiers\\?.lua;.\\Scripts\\modifiers\\drawing\\?.lua;.\\Scripts\\modifiers\\items\\?.lua;.\\Scripts\\modifiers\\leaderMods\\?.lua;.\\Scripts\\modifiers\\perks\\?.lua;.\\Scripts\\modifiers\\smns\\?.lua;.\\Scripts\\modifiers\\smns\\items\\?.lua;.\\Scripts\\modifiers\\smns\\perks\\?.lua;.\\Scripts\\modifiers\\smns\\spells\\?.lua;.\\Scripts\\modifiers\\smns\\units\\?.lua;.\\Scripts\\modifiers\\spells\\?.lua;.\\Scripts\\modifiers\\units\\?.lua;.\\Scripts\\modifiers\\units\\bloodsorcerer\\?.lua;.\\Scripts\\modifiers\\units\\multiplicative_stats\\?.lua;.\\Scripts\\modifiers\\units\\torhoth\\?.lua;.\\Scripts\\modules\\?.lua;.\\Scripts\\modules\\smns\\?.lua;.\\Scripts\\workshop\\?.lua;.\\Scripts\\workshop\\classes\\?.lua"
 require('setValue')
+require('smnsConditions')
 
 function getModifierDisplay(unit, prev)
 	return prev
@@ -9,21 +10,37 @@ function getModifierDescTxt(unit, prev)
 	return prev
 end
 
-function _getHPCoefficient(unit)
-    local maxHP = getScenario():getUnit(unit.id).hpMax
-    if unit.hp / maxHP >= 0.66 then
-        return 0
-    elseif unit.hp / maxHP < 0.66 and unit.hp / maxHP >= 0.33 then
-        return 2
-    elseif unit.hp / maxHP < 0.33 then
-        return 4
-    end
+-- function _getHPCoefficient(unit)
+--     local maxHP = getScenario():getUnit(unit.id).hpMax
+--     if unit.hp / maxHP >= 0.66 then
+--         return 0
+--     elseif unit.hp / maxHP < 0.66 and unit.hp / maxHP >= 0.33 then
+--         return 2
+--     elseif unit.hp / maxHP < 0.33 then
+--         return 4
+--     end
+-- end
+
+-- function getScout(unit, prev)
+-- 	return svFlatEffectScout(unit, prev, 2 - _getHPCoefficient(unit))
+-- end
+
+-- function getMovement(unit, prev)
+-- 	return svFlatEffectMovement(unit, prev, _getHPCoefficient(unit))
+-- end
+
+function getHitPoint(unit, prev)
+	return svFlatEffectHitPoint(unit, prev, 20)
 end
 
-function getScout(unit, prev)
-	return svFlatEffectScout(unit, prev, 2 - _getHPCoefficient(unit))
+function getAttackInitiative(unit, prev)
+	return svFlatEffectInitiative(unit, prev, smnsConditions_getCorpseBonus(unit, 2))
 end
 
-function getMovement(unit, prev)
-	return svFlatEffectMovement(unit, prev, _getHPCoefficient(unit))
+function getAttackDamage(unit, prev)
+	return svFlatEffectDamage1(unit, prev, smnsConditions_getCorpseBonus(unit, 2))
+end
+
+function getAttack2Damage(unit, prev)
+	return svFlatEffectDamage2(unit, prev, smnsConditions_getCorpseBonus(unit, 2))
 end
