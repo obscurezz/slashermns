8934bcb1764dcd4c6ba7e8325819e9af172befe4
obscurezz <iamjustobscure@gmail.com>
2025-07-24 10:12:41 +0300

    rewrote almost all modifiers descriptions

--- Scripts/modifiers/smns/units/GornDefender.lua
+++ Scripts/modifiers/smns/units/GornDefender.lua
@@ -11,24 +11,9 @@ end
 
 function getAttackDamRatio(unit, prev)
 	local Ingenier = Id.new('g070um0031').value
-	_get_Group_and_Mods(unit)
-	
-	
-	if _GroupInfo_stackHasModifierAmount(Ingenier) > 0 then
-	local group = _GroupInfo_getCurrentGroup()
-		local units = group.units
-		local u
-		for i = 1, #units do
-			u = units[i]
-			if u.hp > 0 then
-				local modUnits = _GroupInfo_UnitModifiers(u)
-				if _GroupInfo_UnitModifierAmount(modUnits, Ingenier) > 0 then
-					return 75
-				end
-			end
-		end
-		
+	if smnsConditions_heroCondition(unit, Ingenier) then
+		return prev + 25
 	end
 	return prev
-	
+
 end
