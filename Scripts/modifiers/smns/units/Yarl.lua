1177c7efd0d8db36d5076be63f5ec5c8173f9eab
obscurezz <iamjustobscure@gmail.com>
2025-10-03 08:14:33 +0300

    Yarl rollbacked perk and added debuff immunity

--- Scripts/modifiers/smns/units/Yarl.lua
+++ Scripts/modifiers/smns/units/Yarl.lua
@@ -6,14 +6,18 @@ function getModifierDescTxt(unit, prev)
 	return prev
 end
 
--- function getAttackReach(unit, prev)
--- 	if (unit.hp / unit.hpMax) < 0.5 then
--- 		return 14
--- 	end
+function getAttackReach(unit, prev)
+	if (unit.hp / unit.hpMax) < 0.5 then
+		return 14
+	end
 
--- 	return prev
--- end
+	return prev
+end
 
 function getAttackDamRatio(unit, prev)
-	return prev + 40
+	if (unit.hp / unit.hpMax) < 0.5 then
+		return prev + 50
+	end
+
+	return prev
 end
